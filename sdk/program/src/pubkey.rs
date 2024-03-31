e7e37867452b6ba4915600abb57f8ee5e26579ff
Andrei Silviu Dragnea <andreidragnea.dev@gmail.com>
2024-03-31 19:38:24 +0300

    Fix compile errors

--- sdk/program/src/pubkey.rs
+++ sdk/program/src/pubkey.rs
@@ -168,6 +168,7 @@ pub fn bytes_are_curve_point<T: AsRef<[u8]>>(_bytes: T) -> bool {
     #[cfg(not(target_os = "solana"))]
     {
         curve25519_dalek::edwards::CompressedEdwardsY::from_slice(_bytes.as_ref())
+            .expect("Input slice should have a length of 32")
             .decompress()
             .is_some()
     }
